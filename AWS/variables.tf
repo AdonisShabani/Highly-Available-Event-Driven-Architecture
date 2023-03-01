9dd45d83a844276b1ee099d784892f9c22ab2805
Adonis Shabani <adonis.shabani18@gmail.com>
2023-03-01 21:07:59 +0100

    created myVPC

--- AWS/variables.tf
+++ AWS/variables.tf
@@ -1,22 +1,17 @@
 variable "region" {
   type        = string
   description = "region"
-  default     = "us-central-1"
-
+  default     = "eu-central-1"
 }
 
 variable "access_key" {
   type        = string
   description = "Aws Access Key"
-
-
 }
 
 variable "secret_key" {
   type        = string
   description = "Aws Secret Access Key"
-
-
 }
 
 
