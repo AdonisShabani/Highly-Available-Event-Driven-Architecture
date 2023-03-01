c6b9803f68f099f6416e50c62ee45f4d417086b2
Adonis Shabani <adonis.shabani18@gmail.com>
2023-03-01 23:38:22 +0100

    created a Private routing table and associate with two private subnets

--- AWS/subnets.tf
+++ AWS/subnets.tf
@@ -4,3 +4,24 @@ resource "aws_subnet" "subnet" {
   availability_zone = each.value.availability_zone
   vpc_id            = aws_vpc.vpc.id
 }
+
+
+resource "aws_route_table" "route-tabels" {
+  vpc_id = aws_vpc.vpc.id
+
+  route = []
+
+  tags = {
+    Name = "Private-RT"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_association" {
+  for_each = {
+    for subnet_name, subnet in var.subnets :
+    subnet_name => subnet if contains(["private-subent-1a", "private-subent-1b"], subnet_name)
+  }
+
+  subnet_id = aws_subnet.subnet[each.key].id 
+  route_table_id = aws_route_table.route-tabels.id
+}
--- AWS/variables.tf
+++ AWS/variables.tf
@@ -31,18 +31,22 @@ variable "subnets" {
     "public-subent-1a" = {
       availability_zone = "eu-central-1a"
       cidr_block        = "10.0.1.0/24"
+
     }
     "public-subent-1b" = {
       availability_zone = "eu-central-1b"
       cidr_block        = "10.0.2.0/24"
+
     }
     "private-subent-1a" = {
       availability_zone = "eu-central-1a"
       cidr_block        = "10.0.3.0/24"
+
     }
     "private-subent-1b" = {
       availability_zone = "eu-central-1b"
       cidr_block        = "10.0.4.0/24"
+
     }
   }
 
