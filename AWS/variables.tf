57e1cf70604db87582b7e72e0d508e09664b4ae0
Adonis Shabani <adonis.shabani18@gmail.com>
2023-03-01 20:49:37 +0100

    Added AWS provider

---
+++ AWS/variables.tf
@@ -0,0 +1,26 @@
+variable "region" {
+  type        = string
+  description = "region"
+  default     = "us-central-1"
+
+}
+
+variable "access_key" {
+  type        = string
+  description = "Aws Access Key"
+
+
+}
+
+variable "secret_key" {
+  type        = string
+  description = "Aws Secret Access Key"
+
+
+}
+
+
+variable "AWS_SESSION_TOKEN" {
+  type        = string
+  description = "Aws Session Token"
+}
