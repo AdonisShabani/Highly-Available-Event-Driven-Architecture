9feea14951d7ed616cbfef14b2abc0936913c062
Adonis Shabani <adonis.shabani18@gmail.com>
2023-03-06 00:39:34 +0100

    added Lambda event source mapping and edited iam role policiy

--- Lampda-SQS-SNS/main.tf
+++ Lampda-SQS-SNS/main.tf
@@ -2,40 +2,72 @@ resource "aws_sns_topic" "sns" {
   name = "My-topic"
 }
 
-# Define the SQS queue
 resource "aws_sqs_queue" "sqs" {
   name = "My-queue"
 }
 
-# Subscribe the SQS queue to the SNS topic
 resource "aws_sns_topic_subscription" "subsription" {
   topic_arn = aws_sns_topic.sns.arn
   protocol  = "sqs"
   endpoint  = aws_sqs_queue.sqs.arn
 }
 
-resource "aws_iam_role" "example" {
-  name = "example-role"
+resource "aws_iam_role_policy" "test_policy" {
+  name = "test_policy"
+  role = aws_iam_role.test_role.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes"
+        ]
+        Effect   = "Allow"
+        Resource = aws_sqs_queue.sqs.arn
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Action = "sts:AssumeRole"
         Effect = "Allow"
+        Sid    = ""
         Principal = {
           Service = "lambda.amazonaws.com"
         }
-      }
+      },
     ]
   })
 }
 
-resource "aws_lambda_function" "example" {
+
+resource "aws_lambda_function" "lambda" {
   filename      = "function.zip"
   function_name = "Myfunction"
-  role          = aws_iam_role.example.arn
+  role          = aws_iam_role.test_role.arn
   handler       = "index.handler"
   runtime       = "nodejs14.x"
+}
+
+resource "aws_lambda_event_source_mapping" "map" {
+  event_source_arn = aws_sqs_queue.sqs.arn
+  function_name    = aws_lambda_function.lambda.arn
+
+  depends_on = [
+    aws_iam_role_policy.test_policy
+  ]
 }
 
 
